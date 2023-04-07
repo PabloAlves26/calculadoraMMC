420e567007802dd88641a79859909809eb58213f
PabloAlves26 <pablo26alves@gmail.com>
2023-04-07 01:28:33 -0300

    Erros de portugues corrigidos.

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ function calcular() {
     let text = "";
 
     if (imc < 18.5) {
-        text=(`${nome}, você está muito magro(a)! É importanter mater uma dieta rica em carboidratos, como pães e macarrão`);
+        text=(`${nome}, você está muito magro(a)! É importante manter uma diéta rica em carboidratos, como pães e macarrão`);
     } else if (imc < 24.9) {
-        text=(`${nome}, seu peso está normal. Mantenha uma dieta regular e sempre lembre de calcular o seu IMC para saber como está sua saúde`);
+        text=(`${nome}, seu peso está normal. Mantenha uma diéta regular e sempre lembre de calcular o seu IMC para saber como está sua saúde`);
     } else if (imc < 29.9) {
         text=(`${nome}, você está com sobre-peso! É recomendado que diminua seu peso mantendo uma dieta regular e praticando exercicios fisicos.`);
     } else if (imc < 39.9) {
-        text=(`${nome}, você está com obesidade morbida! Atenção! Sua saúde está em risco. Procure um nutricionista e comece uma diéta já!`);
+        text=(`${nome}, você está com obesidade mórbida! Atenção! Sua saúde está em risco. Procure um nutricionista e comece uma diéta já!`);
     }
     document.getElementById("textarea").innerText=text;
 }
